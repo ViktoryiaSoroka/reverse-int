5cec17408a69fe91dd9bdea8f40ab8d25f1f61de
Viktoryia Soroka <v.soroka@live.com>
2022-02-23 14:27:19 +0200

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let s = String(n);
+    let newStr = s.replace(/[^0-9]/g, );
+    let startMassive = newStr.split('');
+    let revMassive = startMassive.reverse();
+    let revStr = revMassive.join('');
+    let result = parseInt(revStr, 10);
+    return result;
+};
